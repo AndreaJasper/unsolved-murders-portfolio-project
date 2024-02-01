1a68a81e5d04d01a1836f5b0a97e025e215cb4f8
jasper_a <andrea.nicole.jasper@gmail.com>
2024-02-01 16:37:24 -0700

    converts perp age to strings

--- script.py
+++ script.py
@@ -4,27 +4,44 @@ import csv
 
 data_set = "homicide_report.csv"
 
-data_file = []
+perp_age_list = []
 
 with open(data_set, newline='') as data:
   data_reader = csv.DictReader(data)
   for row in data_reader:
-    data_file.append(row)
-
-# with open(data_set, newline='') as data:
-#   reader = csv.reader(data, delimiter=' ')
-#   for row in reader:
-#     data_file.append(', '.join(row))
-
-print(data_file[2])
+    perp_age_list.append(row['Perpetrator Age'])
 
 # how many male v female victims
 
 # how many male v female v unknown perpetrators
 
 # average perpetrator age
+# age_stripped = []
+# for age in perp_age_list:
+#   strip_age = age.strip('')
+#   age_stripped.append(strip_age)
 
-# print("Average age of perpetrator is " + round(average))
+# print(age_stripped)
+
+# def convert_to_int(input_str):
+#   if input_str.isdigit():
+#     return int(input_str)
+#   else:
+#     print("Invlid input.")
+#     return None
+
+perp_age = []
+# converts string numbers back to integers
+# def convert_str_to_int(list):
+for num in perp_age_list:
+  num_str = int(num)
+  perp_age_int = num_str
+
+  perp_age.append(perp_age_int)
+
+  print(perp_age)
+# average_perp_age = perp_age_int/len(perp_age)
+# print("Average age of perpetrator is " + str(average_perp_age))
 
 # state with the most murders
 
