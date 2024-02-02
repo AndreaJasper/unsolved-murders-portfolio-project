0e0a004faec29ea4cb982b2716bf3fc99cae87df
jasper_a <andrea.nicole.jasper@gmail.com>
2024-02-01 17:10:43 -0700

    continued work on perp age

--- script.py
+++ script.py
@@ -11,28 +11,9 @@ with open(data_set, newline='') as data:
   for row in data_reader:
     perp_age_list.append(row['Perpetrator Age'])
 
-# how many male v female victims
 
-# how many male v female v unknown perpetrators
-
-# average perpetrator age
-# age_stripped = []
-# for age in perp_age_list:
-#   strip_age = age.strip('')
-#   age_stripped.append(strip_age)
-
-# print(age_stripped)
-
-# def convert_to_int(input_str):
-#   if input_str.isdigit():
-#     return int(input_str)
-#   else:
-#     print("Invlid input.")
-#     return None
 
 perp_age = []
-# converts string numbers back to integers
-# def convert_str_to_int(list):
 for num in perp_age_list:
   num_str = int(num)
   perp_age_int = num_str
