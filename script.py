75109c586056225c01c2589b7529770601415f91
jasper_a <andrea.nicole.jasper@gmail.com>
2024-02-02 15:20:11 -0700

    began work on crimes by state

--- script.py
+++ script.py
@@ -5,20 +5,27 @@ import csv
 data_set = "homicide_report.csv"
 
 perp_age_list = []
+state = []
+city = []
+year_list = []
+solved = []
+victim_count = []
 
 with open(data_set, newline='') as data:
   data_reader = csv.DictReader(data)
   for row in data_reader:
-    # row['Perpetrator Age'] = int(row['Perpetrator Age'])
     perp_age_list.append(row['Perpetrator Age'])
+    state.append(row['State'])
+    city.append(row['City'])
+    year_list.append(row['Year'])
+    solved.append(row['Crime Solved'])
+    victim_count.append(row['Victim Count'])
 
 
 # locates any age fields that are empty
-# for i, num in enumerate(perp_age_list):
-#     try:
-#         int(num)
-#     except:
-#         print(i, num)
+for i, num in enumerate(perp_age_list):
+    if not num.isdigit():
+        print(i, num)
 
 # changes perpetrator age fromstring to int
 perp_age = []
@@ -41,6 +48,31 @@ print("Average age of perpetrator is " + str(math.floor(average_perp_age)))
 # solved v unsolved
 
 # state with the most
+# print(victim_count)
+# print(state)
+
+def create_dictionary(perp_age_list, state, city, year_list, solved, victim_count):
+  crimes = dict()
+  num_crimes = len(perp_age_list)
+  for i in range(num_crimes):
+    crimes[perp_age_list[i]] : {'Perpetrator Age': perp_age_list[i],
+                                'State': state[i],
+                                'City': city[i],
+                                'Year': year_list[i],
+                                'Crime Solved': solved[i],
+                                'Victim Count': victim_count[i]
+                                }
+  return crimes
+
+crimes = create_dictionary(perp_age_list, state, city, year_list, solved, victim_count)
+print(crimes)
+
+# for row in zipped_state:
+
+
+# victim_to_state = {key: value for key, value in zipped_state}
+# print("Victims per sate are: " + str(victim_to_state))
+
 
 # state with the least
 
