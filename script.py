eab9611965dd09f013b45e5694d06b95eb0522b4
jasper_a <andrea.nicole.jasper@gmail.com>
2024-02-01 17:57:40 -0700

    resolves value eror for average perp age

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 
-import json
+import math
 import csv
 
 data_set = "homicide_report.csv"
@@ -9,20 +9,32 @@ perp_age_list = []
 with open(data_set, newline='') as data:
   data_reader = csv.DictReader(data)
   for row in data_reader:
+    # row['Perpetrator Age'] = int(row['Perpetrator Age'])
     perp_age_list.append(row['Perpetrator Age'])
 
 
+# locates any age fields that are empty
+# for i, num in enumerate(perp_age_list):
+#     try:
+#         int(num)
+#     except:
+#         print(i, num)
 
+# changes perpetrator age fromstring to int
 perp_age = []
 for num in perp_age_list:
-  num_str = int(num)
-  perp_age_int = num_str
-
+  num_int = int(num)
+  perp_age_int = num_int
   perp_age.append(perp_age_int)
 
-  print(perp_age)
-# average_perp_age = perp_age_int/len(perp_age)
-# print("Average age of perpetrator is " + str(average_perp_age))
+# sum of perpetrators ages
+avg_perp_age = 0
+for age in perp_age:
+  avg_perp_age += age
+
+#average perpetrator age
+average_perp_age = avg_perp_age/len(perp_age)
+print("Average age of perpetrator is " + str(math.floor(average_perp_age)))
 
 # state with the most murders
 
